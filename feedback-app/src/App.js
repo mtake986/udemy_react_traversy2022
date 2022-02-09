7fa4eb5e560809bb1b331b739a9d809749902faf
Masahiro Takechi <tak21011@byui.edu>
2022-02-08 21:42:32 -0700

    section 3: components, props, state 1-4

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,6 +1,19 @@
+import {useState} from 'react'
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from './data/FeedbackData'
+
 function App() {
+  // const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const [text, setText] = useState('an example text')
   return (
-    <h1>My app</h1>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
   )
 }
 export default App;
---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dictum risus ut luctus porta. Nulla quis odio eget elit tincidunt suscipit. Praesent convallis erat eget leo ullamcorper consequat. Nunc rutrum neque sit amet efficitur gravida."
+  },
+  {
+    id: 2,
+    rating: 8, 
+    text: "Nullam malesuada interdum purus, at finibus nulla vestibulum eu. Morbi sed accumsan elit, sit amet efficitur tellus. Vestibulum facilisis felis ut eros hendrerit fermentum. Phasellus hendrerit enim in dui dapibus finibus. Proin cursus dolor odio, a pellentesque lorem volutpat vel."
+  },
+  {
+    id: 3,
+    rating: 4,
+    text: "Nulla blandit felis vitae nibh mattis, quis dictum dolor faucibus. Maecenas faucibus convallis quam, nec fringilla metus iaculis vitae. Ut vel porttitor sem.",
+  },
+]
+
+export default FeedbackData
